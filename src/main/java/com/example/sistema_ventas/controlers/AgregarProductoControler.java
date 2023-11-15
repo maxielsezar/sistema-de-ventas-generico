9f50ea15c79b7a9cb1843d7421e39c9855ebce73
franco <frankigiu15@hotmail.com>
2023-11-15 19:47:42 -0300

    trabajando en la ventana de producto

--- src/main/java/com/example/sistema_ventas/controlers/AgregarProductoControler.java
+++ src/main/java/com/example/sistema_ventas/controlers/AgregarProductoControler.java
@@ -5,16 +5,31 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class AgregarProductoControler {
 
     @FXML
     public TextField nombre,marca,precioUnidad;
+    public Button agregar;
+
 
     int id = 0;
 
+    producto Producto;
+
     ObservableList<producto> listaDeProducto = FXCollections.observableArrayList();
+
+    public  void parametros (producto productoActual){
+
+        Producto = productoActual;
+        nombre.setText(productoActual.getNombre());
+        marca.setText(productoActual.getMarca());
+        precioUnidad.setText(String.valueOf(productoActual.getPrecio_x_unidad()));
+        agregar.setText("actualizar");
+        this.id = productoActual.getId_producto();
+    }
 
     public void guardar(){
 
