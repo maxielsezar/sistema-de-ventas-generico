cecd70e1425cc887c6cbf18a3de26ab9e7610e25
franco <frankigiu15@hotmail.com>
2023-11-27 21:21:42 -0300

    se hace la ventana de venta

---
+++ src/main/java/com/example/sistema_ventas/VentanaCajacontroler.java
@@ -0,0 +1,14 @@
+package com.example.sistema_ventas;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class VentanaCajacontroler {
+
+    public TableView tabla_caja;
+
+    public TableColumn fecha;
+    public TableColumn egresos;
+    public TableColumn ingresos;
+
+}
